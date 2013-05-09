b3ad8309c2df3a43634549f13e0142379b23ea5f
Jon <jon@rivingtondigital.com>
2013-05-08 20:27:44 -0400

    next

--- app/store/InstrumentStore.js
+++ app/store/InstrumentStore.js
@@ -13,7 +13,7 @@ Ext.define('ceda.store.InstrumentStore', {
 				id: 1, 
 				name: 'EDA 5',
 				description: [
-								'<p>Test Me! This interview is designed to assist in the assessment of a feeding ',
+								'<p>Test Me 3! This interview is designed to assist in the assessment of a feeding ',
 								'or eating disorder or related conditions in adults according to the DSM-5 ', 
 								'criteria. It is meant to be used by individuals with familiarity with and ', 
 								'training in assessment of eating disorders.</p>',
