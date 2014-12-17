6e4ddddfc8e65aa00198ff9421441d33ecab2b8a
jc <jc@rivingtondigital.com>
2014-12-17 17:18:45 +0000

    Tim content changes

--- app/store/InstrumentStore.js
+++ app/store/InstrumentStore.js
@@ -11,7 +11,7 @@ Ext.define('ceda.store.InstrumentStore', {
 		data: [
 			{
 				id: 1, 
-				name: 'EDA 5',
+				name: 'EDA-5',
 				description: [
 								'<p>This interview is designed to assist in the assessment of a feeding ',
 								'or eating disorder, or a related condition, in an adult according to DSM-5 ', 
@@ -21,6 +21,9 @@ Ext.define('ceda.store.InstrumentStore', {
 								'and the Probe provides questions for the clinician to employ to assess the ',
 								'presence of the Symptom. Clinicians are advised to begin by using the Probes provided. ',
 								'Follow-up questions  to clarify ambiguous responses can be based on clinical judgment. ',
+								'<p>Comments may be added by pressing the Notes button at the top right of each screen. ',
+								'Additional comments will be added to those previously entered, ',
+								'and all comments will be printed in the final report at the conclusion of the interview.',
 								'<p>It is appropriate to utilize whatever clinical information is available, ',
 								'including the individual\'s answers, the clinician\'s observations, ',
 								'and ancillary sources of information (e.g., treatment providers, ',
@@ -29,7 +32,7 @@ Ext.define('ceda.store.InstrumentStore', {
 								'height and weight) whenever possible.</p>',
 								'<p></p> ',
 								'<p></p> ',
-								'<p><em>(Copyright(c) 2013. American Psychiatric Association. ',
+								'<p style="font-size:15px"><em>(v2.001. Copyright &#169 2013. American Psychiatric Association. ',
 								'All rights reserved.)</em></p>'].join(" ")
 			}
 		]
