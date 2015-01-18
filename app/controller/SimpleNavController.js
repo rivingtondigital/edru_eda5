3092b5bd0b7ac1179b2e2ff24dc9b5abf0a82756
Jon <jon@rivingtondigital.com>
2015-01-17 19:24:19 -0500

    Fix back button

--- app/controller/SimpleNavController.js
+++ app/controller/SimpleNavController.js
@@ -275,12 +275,12 @@ Ext.define('ceda.controller.SimpleNavController', {
 
 	backup: function(){
 		qstore = Ext.getStore('questionStore');
-		var old_question = this.questionstack.pop();
+		var old_question = qstore.findRecord('id', this.questionstack.pop());
 		rules = old_question.get('rules');
 		for (var index in rules){
 			var rule = rules[index];
-			global_triggers = this.assessment.triggers;
-			delete global_triggers[rule.trigger];		
+			global_triggers = this.assessment.get('triggers');
+			delete global_triggers[rule.trigger];
 			if (rule.diagnosis){
 				if (typeof this.savedvalues['Diagnosis'] != 'undefined'){
 					delete this.savedvalues['Diagnosis'][rule.diagnosisname];
